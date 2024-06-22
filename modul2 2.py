0c0e7acd7bb6c0953a014a49ef4878b748c2150b
ELENAM1405 <esaulenkoelena@yandex.ru>
2024-06-22 14:39:05 +0300

    Add files via upload

---
+++ modul2 2.py
@@ -0,0 +1,9 @@
+first=input('Введите число: ')
+second=input('Введите число: ')
+third=input('Введите число: ')
+if first==second!=third or second==third!=first or first==third!=second :
+    print('2')
+elif first==second==third:
+    print('3')
+elif first != second != third:
+    print('0')
